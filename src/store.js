77ccaf9db8f0b55dfe649ba674717d561200ed29
Elvan Hidayat <elvanhd@gmail.com>
2023-04-29 19:26:40 +0700

    product list reducer and action

--- src/store.js
+++ src/store.js
@@ -5,8 +5,11 @@ import {
 } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 
 const initialState = {};
 
