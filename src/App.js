3c7d1534d2492eb90c5b76a18612ad9ee6f249d4
Elvan Hidayat <elvanhd@gmail.com>
2023-05-01 06:27:59 +0700

    admin user list screen

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ProductScreen from './screens/ProductScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import UserListScreen from './screens/UserListScreen';
 
 const App = () => {
   return (
@@ -29,6 +30,7 @@ const App = () => {
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
