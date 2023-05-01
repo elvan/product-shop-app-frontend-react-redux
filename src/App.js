c9eba0ae1d0c46c8e3245c75ae7f2c469fe5bef0
Elvan Hidayat <elvanhd@gmail.com>
2023-05-01 07:37:51 +0700

    user edit screen

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import ProductScreen from './screens/ProductScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import UserEditScreen from './screens/UserEditScreen';
 import UserListScreen from './screens/UserListScreen';
 
 const App = () => {
@@ -31,6 +32,7 @@ const App = () => {
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
+          <Route path='/admin/user/:id/edit' component={UserEditScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
